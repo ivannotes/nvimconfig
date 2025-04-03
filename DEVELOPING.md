32dd3cd1aa21804031647ce743ad3bec50702d99
Ivan Lee <miracle.ivanlee@gmail.com>
2025-04-03 14:11:52 -0700

    initial nvim config with nvchad

---
+++ DEVELOPING.md
@@ -0,0 +1,8 @@
+# Developing AmzyiliNvimConfig
+
+Put your notes on developing the package here.
+
+### Using Peru:
+
+* https://builderhub.corp.amazon.com/docs/brazil/user-guide/getting-started-peru.html
+
---
+++ lua/mappings.lua
@@ -0,0 +1,31 @@
+require "nvchad.mappings"
+
+-- add yours here
+
+local map = vim.keymap.set
+
+-- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+map({ "n" }, "<leader>t", "<cmd> NvimTreeToggle <cr>")
+map({ "n" }, "<leader>f", "<cmd> NvimTreeFindFile <cr>")
+
+map({ "n" }, "<leader>r", function()
+  vim.lsp.buf.format({ async = true })
+end, {desc = "Fromat with LSP"})
+
+
+-- remove conflict keymap
+vim.keymap.del('n', '<leader>gt')
+map({ "n" }, "<leader>g", function()
+  vim.lsp.buf.definition()
+end, {desc = "Go to definition"})
+
+map("n", "<F7>", function()
+  vim.fn.setreg('+', vim.fn.expand('%:p'))
+end, { desc = "Copy full file path to clipboard" })
+
+-- configuration
+-- show Lazy.vim configuration panel
+map({ "n" }, "<leader>z", "<cmd> Lazy <cr>")
+
+-- show key bindings
+map({ "n" }, "<leader>k", "<cmd> Telescope keymaps <cr>")
